ada735e70d16c27717924a41cb762ca0054b8c78
thane <thanexor@gmail.com>
2012-06-05 00:15:24 -0500

    Starting to write navigational JS, with swipes and whatnot

--- app/assets/javascripts/top10.js
+++ app/assets/javascripts/top10.js
@@ -1,25 +1,22 @@
 var tmdbkey   = '262ba0731667a5ccd2afa4cf5a34e20f';
 var lastfmkey = '9c5d9862b1419381c2fa9257bd250192';
+var pages = [];
+var currentpage = '';
 
-console.log(tmdbkey);
 $(document).ready(function(){
-	// $('#list').click(function(){
-	// 	// $.getJSON('http://api.themoviedb.org/3/movie/11/images?api_key=262ba0731667a5ccd2afa4cf5a34e20f&callback=1', function(data) {
-	//  //  		console.log(data)
-	// 	// });
-	// 	// return false;
-	// });
+	$('body > .page').each(function(){
+		pages.push($(this).attr('id'));
+	});
 
-	// $.getJSON("http://api.themoviedb.org/3/movie/11/images?callback=",
-	//   {
-	//     api_key: "262ba0731667a5ccd2afa4cf5a34e20f"
-	//   },
-	//   function(data) {
-	//     console.log(data);
-	//   });
 	$('.details').hide();
 	$('figure').click(function(e){
 		$(this).toggleClass('expanded-details');
 		$(this).find('+ .details').slideToggle();
 	});
+	// $('body').swipeRight(function(s){
+
+	// });
+});
+$.pagechange(function(){
+	console.log('page changed');
 });
