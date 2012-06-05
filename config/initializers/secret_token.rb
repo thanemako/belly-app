e25540e8af1b25fe1fd7e2a48954fa666c5aa9f8
thane <thanexor@gmail.com>
2012-06-04 19:56:57 -0500

    Adding all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Belly::Application.config.secret_token = '3b7cb8f647a3e7afc3a2d5965b4d32c5a45c34f7243856823620b16f0f133373a09e87f9b249cb952b1d8794c962c5ce295592c15e529005b7699344b61f3a45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Belly::Application.config.session_store :cookie_store, key: '_belly_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Belly::Application.config.session_store :active_record_store
