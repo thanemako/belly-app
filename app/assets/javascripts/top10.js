b116ae21011611df909fd8dc5be1d4f5c27cdc19
thane <thanexor@gmail.com>
2012-06-05 22:10:20 -0500

    Basic navigation between movie and music pages with swipes or top navigation.

--- app/assets/javascripts/top10.js
+++ app/assets/javascripts/top10.js
@@ -1,22 +1,40 @@
 var tmdbkey   = '262ba0731667a5ccd2afa4cf5a34e20f';
 var lastfmkey = '9c5d9862b1419381c2fa9257bd250192';
-var pages = [];
-var currentpage = '';
+var currentpage;
+var pages;
 
-$(document).ready(function(){
-	$('body > .page').each(function(){
-		pages.push($(this).attr('id'));
+$(document).bind("mobileinit", function(){
+	// overrides default page transition to slide
+	$.extend($.mobile, {
+		defaultPageTransition: 'slide'
 	});
+});
 
+
+$(document).bind("pageinit", function(){
 	$('.details').hide();
-	$('figure').click(function(e){
+	$('figure').click(function(){
 		$(this).toggleClass('expanded-details');
 		$(this).find('+ .details').slideToggle();
 	});
-	// $('body').swipeRight(function(s){
 
-	// });
+	var pages = [];
+	$('body > .page').each(function(){
+		pages.push($(this).attr('id'));
+	});
+	
+	$('body').swipe(function(){
+		if (currentpage == 'movies') {
+			$.mobile.changePage( "#music", { transition: "slide"} );
+		}
+		else {
+			$.mobile.changePage( "#movies", { transition: "slide"} );
+		}
+		
+	});
 });
-$.pagechange(function(){
-	console.log('page changed');
+
+$(document).bind("pagechange", function(){
+	// gets current page ID on page change
+	currentpage = $('.ui-page-active').attr('id');
 });
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
   	@title = "Home"
+
+  	@music = "music"
   end
 end
